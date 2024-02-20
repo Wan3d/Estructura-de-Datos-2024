e0f6507004f0d196c18970c08234d3ee49eed4a9
Wan3d <128077087+Wan3d@users.noreply.github.com>
2024-02-19 22:42:48 -0600

    Factorial

---
+++ FactorialRecursividad.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+char r;
+int factorial(int n, int mult, int resultado)
+{
+	if (n == 0)
+	{
+	n=1;
+	return 1;
+	}
+	else
+	{
+	mult = n * mult;
+	cout<<n<<" * "<<mult/n<<" = "<<mult<<endl;
+	return factorial(n-1,mult,resultado);
+	}
+}
+int main(int argc, char *argv[]) {
+	do{
+	system("cls");
+	int n,mult=1,resultado;
+	cout<<"Ingrese un número: ";
+	cin>>n;
+	cout<<endl;
+	factorial(n,mult,resultado);
+	cout<<endl;
+	cout<<endl;
+	cout<<"¿Desea repetir el proceso nuevamente? (y/n): ";
+	cin>>r;
+	} while (r == 'Y' || r == 'y');
+	return 0;
+}
+
